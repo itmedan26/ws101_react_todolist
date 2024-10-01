e487f94018620bfb2fd6eb25894fdcb51760dc8a
itmedan26 <danielpunzalan2003@gmail.com>
2024-10-01 10:33:27 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import './App.css';
 import Navbar from './components/Navbar/navbar';  
-import Home from './components/Home/hero';
-import About from './components/About/about';
-import Contact from './components/Contact/contact';
-import Footer from './components/Footer/footer';
-import HandlingClass from './components/Hero/hero';
-import HandingFunction from './components/HandingFunctions';
+// import Home from './components/Home/home';
+// import About from './components/About/about';
+// import Contact from './components/Contact/contact';
+// import Footer from './components/Footer/footer';
+// import HandingFunction from './components/HandingFunctions';
 // import Biodata from './Biodata/biodata';
 // import ToDoList from './ToDoListApp/todolist';
 
 function App() {
   return (
-    <div className="biodata">
-      *<Navbar name="Daniel" lastname="Punzalan" />
+    <div className="Navbar">
+       {/*<Navbar name="Daniel" lastname="Punzalan" />
       <Home message="ILY"/>
       <About />
       <Contact />
       <Footer />
       <HandingFunction />
       <HandlingClass />
-      {/*<Biodata /
+      <Biodata /
       <ToDoList />*/}
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import './navbar.css';
 
-const NavBar = () => {
-    return (
-        <nav style={{ backgroundColor: '#333', padding: '15px' }}>
-            <Link to="/" style={{ color: 'white', margin: '10px' }}>Home</Link>
-            <Link to="/about" style={{ color: 'white', margin: '10px' }}>About</Link>
-            <Link to="/contact" style={{ color: 'white', margin: '10px' }}>Contact Us</Link>
-        </nav>
-    );
+const Navbar = () => {
+  return (
+
+<nav className="navbar">
+  <div className="navbar-left">
+    <a href="/" className="logo">Daniel-Shop</a>
+  </div>
+  <div className="navbar-center">
+    <ul className="nav-links">
+      <li>
+        <a href="/home">Home</a>
+      </li>
+      <li>
+        <a href="/about">About Us</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+  <div className="navbar-right">
+    <a href="/cart" className="cart-icon">
+      <i className="fas fa-shopping-cart"></i>
+      <span className="cart-count">Sign up</span>
+    </a>
+    <a href="/account" className="user-icon">
+      <i className="fas fa-user"></i>
+    </a>
+  </div>
+</nav>
+);
 };
 
-export default NavBar;
+export default Navbar;
